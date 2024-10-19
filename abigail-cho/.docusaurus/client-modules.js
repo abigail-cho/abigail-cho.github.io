453d5ab8f6b8bf5505248d273284a3266e0c807f
abigail-cho <abscho@gmail.com>
2024-10-19 01:02:40 -0400

    TRYING

---
+++ abigail-cho/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\absch\\Documents\\portfolio\\abigail-cho\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\absch\\Documents\\portfolio\\abigail-cho\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\absch\\Documents\\portfolio\\abigail-cho\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\absch\\Documents\\portfolio\\abigail-cho\\src\\css\\custom.css"),
+];
