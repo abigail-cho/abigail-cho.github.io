8c52696d6e1bb5502b1700bbfd0758785d9088bc
abigail-cho <abscho@gmail.com>
2024-10-19 01:43:01 -0400

    update config

--- abigail-cho/docusaurus.config.ts
+++ abigail-cho/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: '/img/notebook.png',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://abigail-cho.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
