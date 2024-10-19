c2103372f5bfb5df2a9abd900fa3ad7af1ef9a38
abigail-cho <abscho@gmail.com>
2024-10-19 00:41:24 -0400

    Revert "revert changes"
    This reverts commit 96e693c356f3de3d4d2ec6cce5838b4b70e1307e.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\absch\\Documents\\portfolio\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\absch\\Documents\\portfolio\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\absch\\Documents\\portfolio\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\absch\\Documents\\portfolio\\src\\css\\custom.css"),
-];
