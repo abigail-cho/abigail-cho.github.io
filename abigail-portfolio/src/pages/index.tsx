0b8b94d47ddc5ef1486c81c5d8fe41d7bc0f3753
abigail-cho <abscho@gmail.com>
2024-10-18 21:55:10 -0400

    fixed flex finally, updated intro and colors

--- abigail-portfolio/src/pages/index.tsx
+++ abigail-portfolio/src/pages/index.tsx
@@ -33,10 +33,25 @@ function Headshot() {
 function Intro() { 
   const {siteConfig} = useDocusaurusContext();
   return (
-    <div>
-      <p className={clsx('introduction', styles.intro)}>
-        Welcome to my portfolio! I've been a technical writer for 5 years
-      </p>
+    <div className={clsx('introduction', styles.intro)}>
+      <p>
+      Welcome to my portfolio! I'm an expert technical writer with 5+ years of experience in government, SaaS, and cybersecurity. I have produced a variety of technical documentation, including:</p>
+      <ul>
+        <li>Product documentation</li>
+        <li>API documentation</li>
+        <li>SDK documentation</li>
+        <li>Proposals</li>
+      </ul>
+      <p>In addition to documentation, I have a proven track record in joining organizations as the first or lone tech writer and building out technical writing teams from the ground up. Some examples of technical writing practices I've established are:</p>
+      <ul>
+        <li>Integrating technical writing into the product lifecycle</li>
+        <li>Standardizing the types of content that belong in documentation</li>
+        <li>Leveraging knowledge base analytics for data-driven documentation maintenance</li>
+        <li>Migrating an organization to a docs-as-code workflow</li>
+        <li>Publishing and maintaining an organization's first technical writing style guide</li>
+      </ul>
+      <p>If you'd like to connect, feel free to email me at <a href="mailto:abscho@gmail.com">abscho@gmail.com</a> or send me a message on <a href="https://www.linkedin.com/in/abscho/">LinkedIn.</a></p>
+      <p>I am currently located in <b>Germantown, MD, USA.</b></p>
     </div>
   )
 }
@@ -49,8 +64,8 @@ export default function Home(): JSX.Element {
       description="Abigail Cho's technical writing portfolio">
       <HomepageHeader />
       <div className={clsx('intro box', styles.introBox)}>
-        <Intro />
-        <Headshot />
+        <div className={clsx('intro', styles.intro)}><Intro /></div>
+        <div className={clsx('intro', styles.propic)}><Headshot /></div>
       </div>  
     </Layout>
   );
