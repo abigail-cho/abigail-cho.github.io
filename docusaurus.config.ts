d392afb9a2cf7c5dae99105fd83b77cec6a752e6
abigail-cho <abscho@gmail.com>
2024-10-18 22:47:51 -0400

    add deploymentbranch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,6 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'abigail-cho', // Usually your GitHub org/user name.
   projectName: 'abigail-cho.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
