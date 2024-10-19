4d99d7b94994c53a137ef927e19f9996ac479bf9
abigail-cho <abscho@gmail.com>
2024-10-19 00:41:31 -0400

    Revert "Deploy website - based on 7ec926b04ea191cd119484b8e881f8f8dc329186"
    This reverts commit 404dc2965480d565bc04a6db10397ede25ef3bcd.

--- assets/js/c15d9823.10450cb8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkabigail_portfolio=self.webpackChunkabigail_portfolio||[]).push([[146],{9328:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
